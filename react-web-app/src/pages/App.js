638e3a2dc3906dcbae5c773f61de0b5150863134
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-06 17:52:51 -0700

    Website

---
+++ react-web-app/src/pages/App.js
@@ -0,0 +1,15 @@
+import '../style/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Bunky
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

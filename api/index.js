f8361c9d084ff84b626c787c392c881ce6cd25a5
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-08 16:17:48 -0700

    API

---
+++ api/index.js
@@ -0,0 +1,16 @@
+// Create a express app with port 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route for the root path
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+module.exports = app;

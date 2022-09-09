36aea2fc35345177a2c67bf8f1b04998cce0121f
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-09 00:36:30 -0700

    Encryption

--- aws-rest-api/handler.js
+++ aws-rest-api/handler.js
@@ -4,6 +4,9 @@ const axios = require("axios");
 const dotenv = require("dotenv");
 var bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var CryptoJS = require("crypto-js");
+const UIDGenerator = require('uid-generator');
+const uidgen = new UIDGenerator();
 
 dotenv.config();
 
@@ -25,12 +28,50 @@ app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 
+// generate new encrypt password
+function genPass() {
+  var hashBase = CryptoJS.lib.WordArray.random(128 / 8);
+
+  var hashPassword = CryptoJS.SHA3(hashBase).toString();
+  var encryptPass = CryptoJS.SHA3(hashPassword).toString();
+
+  return encryptPass;
+}
+
+// Encrypt data
+function encryptData(data, pass) {
+  var encryptedData = CryptoJS.AES.encrypt(data, pass).toString();
+  return encryptedData;
+}
+
+// Decrypt data
+function decryptData(data, pass) {
+  var bytes  = CryptoJS.AES.decrypt(data, pass);
+  var originalText = bytes.toString(CryptoJS.enc.Utf8);
+  return originalText;
+}
+
+// Get Master Password
+function getMasterPass() {
+  const masterPass = process.env.MASTER_PASSWORD;
+  return masterPass;
+}
+
+// Generate session data
+function genSessionId() {
+  var sessionId = uidgen.generateSync(64);
+  return sessionId;
+}
+
 // base route
-app.get("/", (req, res, next) => {
+app.get("/", async (req, res) => {
   return res.status(200).json({
     message: "Hello World!",
+    db: await db.collection('users').findOne()
   });
 });
+
+
 
 // phone auth route
 app.post('/auth/send_phone_code', (req, res) => {
@@ -61,8 +102,11 @@ app.post('/auth/send_phone_code', (req, res) => {
 
 })
 
+
+
 // Verify a code
-app.post('/auth/verify_phone_code', (req, res) => {
+app.post('/auth/verify_phone_code', async (req, res) => {
+
   // Get the number from body
   const { number, code } = req.body;
 
@@ -73,10 +117,58 @@ app.post('/auth/verify_phone_code', (req, res) => {
     .then(verification_check => {
       // Send the response
       if (verification_check.status === 'approved') {
-        return res.status(200).json({
-          message: 'Verification code sent',
-          verification_check
-        })
+
+        const encryptPass = genPass();
+
+        // save user Data
+        const user = {
+          uId: uidgen.generateSync(),
+          phoneNumber: encryptData(number, encryptPass),
+          isVerified: false,
+          encryptPassword: encryptData(encryptPass, getMasterPass()),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          pictures: [],
+          likes: [],
+          dislikes: [],
+          matches: [],
+        }
+
+        const sessionData = {
+          uId: user.uId,
+          sessionId: genSessionId(),
+        }
+
+        const clientSesionData = {
+          sessionId: sessionData.sessionId,
+        }
+        
+        db.collection("users").insertOne(user, function(err, docRes) {
+          if (err) {
+            return res.status(500).json({
+              message: 'Error saving user',
+              err
+            })
+          };
+
+          db.collection("sessions").insertOne(sessionData, function(err, docRes) {
+            if (err) {
+              return res.status(500).json({
+                message: 'Error saving session',
+                err
+              })
+            };
+
+            return res.status(200).json({
+              message: 'User verified and created',
+              sessionData: clientSesionData,
+              verification_check
+            });
+          
+          });
+
+        });
+
       } else if(verification_check.status === 'pending') {
         return res.status(403).json({
           message: 'Invalid Code',
@@ -87,6 +179,27 @@ app.post('/auth/verify_phone_code', (req, res) => {
   
 })
 
+// fetch user data
+app.get('/fetch/user', async (req, res) => {
+  // Get the id from body
+  const { uId } = req.query;
+
+  // get data from mongo
+  const doc = await db.collection("users").findOne( { uId: uId } )
+
+  if (doc === null) {
+    return res.status(404).json({
+      message: 'User not found',
+    })
+  }
+    
+  return res.status(200).json({
+    message: 'User fetched',
+    data: doc,
+    masterPass: encryptData(getMasterPass(), doc.encryptPassword)
+  });
+})
+
 // Set 404 Page
 app.use((req, res, next) => {
   return res.status(404).json({
@@ -95,8 +208,8 @@ app.use((req, res, next) => {
 });
 
 // // Run the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
 
 module.exports.handler = serverless(app);

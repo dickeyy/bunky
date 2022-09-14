90296a580da98032cf38bc8694501c01d3a1f090
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-14 01:29:45 -0700

    Added Google Analytics

--- react-web-app/src/app.js
+++ react-web-app/src/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 // Pages
 import HomePage from './pages/HomePage';
 import PageNotFound from './pages/PageNotFound';
 
+const TRACKING_ID = "G-XGZR7JX9MT"; 
+ReactGA.initialize(TRACKING_ID);
+
 export default function App() {
+    React.useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
     return (
         <BrowserRouter>
             <Routes>

53b6cb3f0809528c3aebcb2d83bbeaa4960ef343
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-09-14 22:19:51 -0700

    Update app.js

--- react-web-app/src/app.js
+++ react-web-app/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import ReactGA from 'react-ga';
+import ReactGa4 from 'react-ga4';
 
 // Pages
 import HomePage from './pages/HomePage';
@@ -8,8 +9,15 @@ import PageNotFound from './pages/PageNotFound';
 
 export default function App() {
     React.useEffect(() => {
-        ReactGA.initialize("G-XGZR7JX9MT");
+        ReactGA.initialize("UA-241148324-1");
         ReactGA.pageview(window.location.pathname + window.location.search);
+
+        ReactGa4.initialize("G-KGKNY8Y1KN");
+        ReactGa4.send("pageview", {
+            page_title: document.title,
+            page_location: window.location.href,
+            page_path: window.location.pathname,
+        });
     }, []);
     return (
         <BrowserRouter>
